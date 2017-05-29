703d82c1784611c26759182d7c40ad36d29245f1
ADAM KELLY <adamkelly@ADAMs-iMac.local>
2017-05-29 18:50:03 -0500

    add players pt.1

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 
 var PLAYERS = [
   {
-    name: "Demo Player #1",
+    name: "Demo Player",
     score: 10,
     id: 1,
   },
@@ -15,11 +15,72 @@ var PLAYERS = [
     id: 2,
   },
   */
-]
+];
+
+var AddPlayerForm = React.createClass({
+  propTypes: {
+    onAdd: PropTypes.func.isRequired,
+  },
+
+  getInitialState: function() {
+    return {
+      name: "",
+    };
+  },
+
+  onNameChange: function(e) {
+    this.setState({name: e.target.value});
+  },
+
+  onSubmit: function(e) {
+    e.preventDefault();
+
+    this.props.onAdd(this.state.name);
+    this.setState({name: ""});
+  },
+
+  render: function() {
+    return (
+      <div className="add-player-form">
+        <form onSubmit={this.onSubmit}>
+          <input type="text" value={this.state.name} onChange={this.onNameChange} />
+          <input type="submit" value="Add Player" />
+        </form>
+      </div>
+    );
+  }
+});
+
+function Stats(props) {
+  var totalPlayers = props.players.length;
+  var totalPoints = props.players.reduce(function(total, player) {
+    return total + player.score;
+  }, 0);
+
+  return (
+    <table className="stats">
+      <tbody>
+        <tr>
+          <td>Players:</td>
+          <td>{totalPlayers}</td>
+        </tr>
+        <tr>
+          <td>Total Points:</td>
+          <td>{totalPoints}</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
+
+Stats.propTypes = {
+  players: PropTypes.array.isRequired,
+};
 
 function Header(props) {
   return (
     <div className="header">
+      <Stats players={props.players} />
       <h1>{props.title}</h1>
     </div>
   );
@@ -27,6 +88,7 @@ function Header(props) {
 
 Header.propTypes = {
   title: PropTypes.string.isRequired,
+  players: PropTypes.array.isRequired,
 };
 
 function Counter(props) {
@@ -93,10 +155,17 @@ var Application = React.createClass({
     this.setState(this.state);
   },
 
+  onPlayerAdd: function(name) {
+    console.log('player added:', name);
+    this.state.players.push({
+      
+    });
+  },
+
   render: function() {
     return (
       <div className="scoreboard">
-        <Header title={this.props.title} />
+        <Header title={this.props.title} players={this.state.players} />
 
         <div className="players">
           {this.state.players.map(function(player, index) {
@@ -109,6 +178,7 @@ var Application = React.createClass({
             );
           }.bind(this))}
         </div>
+        <AddPlayerForm onAdd={this.onPlayerAdd} />
       </div>
     );
   }
